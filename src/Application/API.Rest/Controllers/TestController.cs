befb1b3837082b8645fabe4a4fe708f5fbdfdfd9
emenson Lima Germano <emensonlima@outlook.com.br>
2023-03-08 17:17:48 -0300

    Commit: Aplicação console para ver o tempo de resposta de métodos async e sync.

---
+++ src/Application/API.Rest/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Rest.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    // GET api/values
+    [HttpGet]
+    // Synchronous
+    public IEnumerable<string> Get()
+    {
+        Thread.Sleep(5000);
+        return new string[] { "value1", "value2" };
+    }
+
+    // Asynchronous
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public async Task<IEnumerable<string>> Get(int id)
+    {
+        await Task.Delay(5000);
+        return new string[] { "value1", "value2" };
+    }
+}
---
+++ src/Application/API.Rest/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+int workerThreads, ioThreads;
+ThreadPool.GetMaxThreads(out workerThreads, out ioThreads);
+ThreadPool.SetMaxThreads(Environment.ProcessorCount, ioThreads);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
